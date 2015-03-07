a675ac26b939f46959cbd8ba79e1f59a7462af73
Sam Jesso <samuel.jesso@gmail.com>
2015-03-06 21:15:54 -0400

    Clean up indirect memory addressing.

--- controller.vhd
+++ controller.vhd
@@ -301,35 +301,23 @@ begin
 				when Start_Indirect_Memory_Access =>
 					RFr1a_ctrl <= IR_word(11 downto 8); -- Get the address stored in R1.
 					RFr1e_ctrl <= '1';	                -- Enable port 1 on register file for reading.
-					-- The value in R1 is now available on the RFr1 bus.
-					-- RFr1 bus is connected to RFr1out_dp bus.
-					-- RFr1out_dp bus is connected to dpdata_out bus.
-					-- Next, we need to wire the dpdata_out bus to the maddr_in bus:
 					Ms_ctrl <= "00";
-					-- Now the memory unit sees the value from R1 on its address bus.
-					-- Finally, signal that we intent to read data:
-					Mre_ctrl <= '1';
+					RFs_ctrl <= "01";
 					state <= Write_Indirect_Memory_Access;
 					
 				when Write_Indirect_Memory_Access =>
 					RFr1e_ctrl <= '0';
-					-- At this point, the data at the address specified by R1 is now on the data_out bus of the memory unit.
-					-- data_out is connected to mem_data bus (and IR bus, but we don't care)
-					-- mem_data is option "01" on the smallmux unit.
-					-- We want to connect the mem_data bus to the RFw bus:
-					RFs_ctrl <= "01";
-					-- Done.
+					Mre_ctrl <= '1';
+					RFwa_ctrl <= IR_word(7 downto 4);
 					state <= Save_Indirect_Memory_Access;
 					
 				when Save_Indirect_Memory_Access =>
-					-- Now we simply need to instruct our register file to write the value on the bus into the appropriate register:
-					RFwa_ctrl <= IR_word(7 downto 4);
 					RFwe_ctrl <= '1';
+					Mre_ctrl <= '0';
 					state <= Indirect_Memory_Access_Cleanup;
 					
 				when Indirect_Memory_Access_Cleanup =>
 					RFwe_ctrl <= '0';
-					Mre_ctrl <= '0';
 					state <= S1;
 -- END EXECUTE STAGE
 			  when others =>
--- memory.vhd
+++ memory.vhd
@@ -35,41 +35,41 @@ begin
 	begin
 		if rst='1' then
 			-- test increments and decrement instructions
---			tmp_ram <= (
---				0 => "0011000100001111", -- R1 <- 0x0F       (R1 is a pointer to the address 0x0F)
---				1 => "0011001000110011", -- R2 <- 0x33       (R2 holds a test value)
---				2 => "0001001000001111", -- MEM[0x0F] <- R2  (Write R2 to memory)
---				3 => "1011000100110000", -- R3 <- MEM[R1]    (Use pointer to read value into R3)
---				4 => "0001001100010000", -- MEM[0x10] <- R3  (Save R3 into next address, 0x10)
---				5 => "0111000000010000", -- OUTPUT MEM[0x10] (should be 33)
---				6 => "1111000000000000",
---				others => "0000000000000000"
---			);
+			tmp_ram <= (
+				0 => "0011000100001111", -- R1 <- 0x0F       (R1 is a pointer to the address 0x0F)
+				1 => "0011001000110011", -- R2 <- 0x33       (R2 holds a test value)
+				2 => "0001001000001111", -- MEM[0x0F] <- R2  (Write R2 to memory)
+				3 => "1011000100110000", -- R3 <- MEM[R1]    (Use pointer to read value into R3)
+				4 => "0001001100010000", -- MEM[0x10] <- R3  (Save R3 into next address, 0x10)
+				5 => "0111000000010000", -- OUTPUT MEM[0x10] (should be 33)
+				6 => "1111000000000000",
+				others => "0000000000000000"
+			);
 			
 			-- generate first 7 factorials
-			tmp_ram <= (
-				0  => x"3000", -- MOV4 R0, 0x00
-				1  => x"3150", -- MOV4 R1, 0x50
-				2  => x"3201", -- MOV4 R2, 0x01
-				3  => x"3300", -- MOV4 R3, 0x00
-				4  => x"3401", -- MOV4 R4, 0x01
-				5  => x"214F", -- MOV3 R1, R4
-				6  => x"4045", -- ADD R0, R4, R5
-				7  => x"4323", -- ADD R3, R2, R3
-				8  => x"8354", -- MULT R3, R5, R4
-				9  => x"4121", -- ADD R1, R2, R1
-				10 => x"0656", -- MOV1 R6, 0x56
-				11 => x"6605", -- JZ R6, 0x05
-				12 => x"7050", -- OUT 0x50
-				13 => x"7051", -- OUT 0x51
-				14 => x"7052", -- OUT 0x52
-				15 => x"7053", -- OUT 0x53
-				16 => x"7054", -- OUT 0x54
-				17 => x"7055", -- OUT 0x55
-				18 => x"7056", -- OUT 0x56
-				19 => x"F000", -- HALT
-				others => x"0000"
-			);
+--			tmp_ram <= (
+--				0  => x"3000", -- MOV4 R0, 0x00
+--				1  => x"3150", -- MOV4 R1, 0x50
+--				2  => x"3201", -- MOV4 R2, 0x01
+--				3  => x"3300", -- MOV4 R3, 0x00
+--				4  => x"3401", -- MOV4 R4, 0x01
+--				5  => x"214F", -- MOV3 R1, R4
+--				6  => x"4045", -- ADD R0, R4, R5
+--				7  => x"4323", -- ADD R3, R2, R3
+--				8  => x"8354", -- MULT R3, R5, R4
+--				9  => x"4121", -- ADD R1, R2, R1
+--				10 => x"0656", -- MOV1 R6, 0x56
+--				11 => x"6605", -- JZ R6, 0x05
+--				12 => x"7050", -- OUT 0x50
+--				13 => x"7051", -- OUT 0x51
+--				14 => x"7052", -- OUT 0x52
+--				15 => x"7053", -- OUT 0x53
+--				16 => x"7054", -- OUT 0x54
+--				17 => x"7055", -- OUT 0x55
+--				18 => x"7056", -- OUT 0x56
+--				19 => x"F000", -- HALT
+--				others => x"0000"
+--			);
 		else
 			if (clock'event and clock = '1') then
 				if (Mwe ='1' and Mre = '0') then
